25464c0e8379f82266998d083b51517b645167f7
SylvainLangler <sylvain.langler@etu.unice.fr>
2019-02-20 20:52:28 +0100

    Scène d'accueil en 1280x720

--- src/sample/View.java
+++ src/sample/View.java
@@ -1,10 +1,10 @@
 package sample;
 
 public class View {
-    public static final String XML_ACCUEIL = "resources/Accueil.fxml";
+    public static final String XML_ACCUEIL = "resources/Accueil_1280.fxml";
     public static final String XML_CREERMENU = "resources/CreerMenu.fxml";
     public static final String XML_LISTECOURSEVALIDATION = "resources/Liste_de_course_validation.fxml";
     public static final String XML_MENUAFFICHAGE = "resources/MenuAffichage.fxml";
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 400;
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
 }
