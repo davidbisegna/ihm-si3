8972b1ff364c99f7f1e42b4cb53fa63e598a428a
SylvainLangler <sylvain.langler@etu.unice.fr>
2019-02-23 15:23:22 +0100

    Renommage accueil, ajout CreerMenu.fxml en 1280px

--- src/sample/View.java
+++ src/sample/View.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class View {
-    public static final String XML_ACCUEIL = "resources/Accueil_1280.fxml";
+    public static final String XML_ACCUEIL = "resources/Accueil.fxml";
     public static final String XML_CREERMENU = "resources/CreerMenu.fxml";
     public static final String XML_LISTECOURSEVALIDATION = "resources/Liste_de_course_validation.fxml";
     public static final String XML_MENUAFFICHAGE = "resources/MenuAffichage.fxml";
