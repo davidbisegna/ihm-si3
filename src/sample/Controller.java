d4bd7af52c507128678b201629f8e7f310617d3e
SylvainLangler <sylvain.langler@etu.unice.fr>
2019-02-24 18:25:30 +0100

    Lien entre les pages accueil, Menus et CréerMenu ajout de fx:controller="sample.Controller" et d'un onAction="Load_function" dans les fxml concernés

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,44 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static sample.View.XML_ACCUEIL;
+import static sample.View.XML_CREERMENU;
+import static sample.View.XML_MENUAFFICHAGE;
+
 public class Controller {
+
+    public void load_accueil(ActionEvent event) throws IOException{
+        Parent accueil_parent = FXMLLoader.load(getClass().getResource(XML_ACCUEIL));
+        Scene accueil = new Scene(accueil_parent);
+
+        Stage window = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        window.setScene(accueil);
+        window.show();
+    }
+
+    public void load_menu_affichage(ActionEvent event) throws IOException {
+        Parent menus_parent = FXMLLoader.load(getClass().getResource(XML_MENUAFFICHAGE));
+        Scene menus = new Scene(menus_parent);
+
+        Stage window = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        window.setScene(menus);
+        window.show();
+    }
+
+    public void load_creer_menu(ActionEvent event) throws IOException{
+        Parent creer_menu_parent = FXMLLoader.load(getClass().getResource(XML_CREERMENU));
+        Scene creer_menu = new Scene(creer_menu_parent);
+
+        Stage window = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        window.setScene(creer_menu);
+        window.show();
+    }
 }
